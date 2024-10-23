0bc4084d7dcbf91dddb5b5c6c6612f1b09fe6ccc
Jou Okuyama <ojoxux@gmail.com>
2024-10-24 00:55:30 +0900

    fix: appへの依存を削除

--- line_bot.py
+++ line_bot.py
@@ -1,10 +1,10 @@
+import logging
 from linebot.v3.webhooks import (
     MessageEvent,
     TextMessageContent
 )
 from linebot.v3.webhook import WebhookHandler
 from github_checker import GitHubCommitChecker
-from app import app
 import os
 
 CHANNEL_SECRET = os.environ.get('LINE_CHANNEL_SECRET')
@@ -15,7 +15,7 @@ user_ids = set()
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
     user_id = event.source.user_id
-    app.logger.info(f"Received message: {event.message.text} from user: {user_id}")
+    logging.info(f"Received message: {event.message.text} from user: {user_id}")
     
     user_ids.add(user_id)
 
@@ -23,5 +23,5 @@ def handle_message(event):
     if event.message.text == "コミット確認":
         checker.check_and_notify(user_id, notify_immediately=True)
     else:
-        app.logger.info("条件に一致しないメッセージを受信しました")
+        logging.info("条件に一致しないメッセージを受信しました")
         checker.send_message(user_id, "申し訳ありませんが、そのメッセージは認識できません。'コミット確認'と入力してください。")
